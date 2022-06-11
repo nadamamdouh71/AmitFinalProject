23fd7c05d682254f943263c9ae751ea8c8b701a5
mohammed sobh <mohammed.sobh696@gmail.com>
2022-06-11 12:20:53 +0200

    frist change

---
+++ AutoMotive/AutoMotive/main.c
@@ -0,0 +1,18 @@
+/*
+ * AutoMotive.c
+ *
+ * Created: 6/11/2022 12:01:23 PM
+ * Author : eng_s
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
