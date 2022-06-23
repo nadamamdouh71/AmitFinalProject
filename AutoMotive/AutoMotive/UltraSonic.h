df6b2e17a1e634631089e983a4b621524b53eb5c
nadamamdouh71 <99555194+nadamamdouh71@users.noreply.github.com>
2022-06-23 11:58:02 +0200

    Update UltraSonic.h

--- AutoMotive/AutoMotive/UltraSonic.h
+++ AutoMotive/AutoMotive/UltraSonic.h
@@ -5,12 +5,25 @@
  *  Author: eng_s
  */ 
 #include "STD_types.h"
-
 #ifndef ULTRASONIC_H_
 #define ULTRASONIC_H_
 
-void UltraSonic_Init(void);
-Uint8 UltraSonicDistance(void);
+#include<avr/io.h>
+#include <avr/interrupt.h>
+#include<string.h>
+#include <stdlib.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include "timer/timer.h"
+
+
+#define   TRIGER_DDR    DDRD
+#define   ECHO_DDR      DDRD
+#define   TRIGER_PORT   PORTD
+#define   ECHO_PULLUP   PORTD
+#define   TRIGER        0
+#define   ECHO          3
+
 
 
 
